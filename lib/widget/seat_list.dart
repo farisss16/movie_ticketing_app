66ce0508168c7c52b34ea222587628962be33f95
farisss16 <farisanas81@gmail.com>
2022-10-24 02:15:22 +0700

    third commmit

--- lib/widget/seat_list.dart
+++ lib/widget/seat_list.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors_in_immutables, non_constant_identifier_names, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class SeatWidget extends StatelessWidget {
